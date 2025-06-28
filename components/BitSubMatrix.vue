d8cb6ed562affd2d841766405e920478f19a5b86
Peter Blackson <pioczarn@gmail.com>
2025-06-28 16:29:27 +0200

    Update for fixes to the recognizer

---
+++ components/BitSubMatrix.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>predicted symbols</h2>
+    <ul>
+        <template v-for="(ch, index) of content.row.trim()">
+            <li v-if="ch === '1'">
+                {{ names[index] }}
+            </li>
+        </template>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['content', 'names']
+}
+</script>
+
+<style>
+</style>
